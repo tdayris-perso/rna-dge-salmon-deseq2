489cb2bd47789eab86b99618b9cf7e583135431b
tdayris-perso <tdayris@tutanota.de>
2020-11-09 14:21:24 +0100

    [tests] (unittesting): New tests for common functions

--- scripts/common_script_rna_dge_salmon_deseq2.py
+++ scripts/common_script_rna_dge_salmon_deseq2.py
@@ -10,8 +10,10 @@ this pipeline.
 
 import argparse        # Argument parsing
 import itertools       # Handle iterators and comprehensions
-import pandas          # Handle large datasets
 import os              # OS related operations
+import pandas          # Handle large datasets
+import pytest          # Unit testing
+import shlex           # Lexical analysis
 import yaml            # Handle Yaml IO
 
 from pathlib import Path                             # Easily handle paths
@@ -42,7 +44,16 @@ def get_gtf_path(config: Dict[str, Any]) -> Dict[str, str]:
     return f"genomes/{os.path.basename(config['ref']['gtf'])}"
 
 
-def get_condition_dict_w(factor: Any, design) -> Dict[str, str]:
+def test_get_gtf_path() -> None:
+    """
+    This function tests the get_gtf_path function above
+    """
+    expected = "genomes/test.gtf"
+    tested = get_gtf_path({'ref': {'gtf': 'test.gtf'}})
+    assert expected == tested
+
+
+def get_condition_dict_w(factor: Any, design: pandas.DataFrame) -> Dict[str, str]:
     """
     Return a dictionnary with:
     sample_id : condition
@@ -50,11 +61,25 @@ def get_condition_dict_w(factor: Any, design) -> Dict[str, str]:
     return dict(zip(design["Sample_id"], design[factor]))
 
 
+def test_get_condition_dict_w() -> None:
+    """
+    Test the function get_condition_dict_w above
+    """
+    expected = {"name1": "factor1", "name2": "factor1", "name3": "factor2"}
+    tested_data = pandas.DataFrame(
+        {"my_factor": ["factor1", "factor1", "factor2"],
+         "Sample_id": ["name1", "name2", "name3"],
+         "other": ["other1", "other1", "other2"]}
+    )
+    tested = get_condition_dict_w("my_factor", tested_data)
+    assert expected == tested
+
+
 def get_intgroups(design: pandas.DataFrame,
                   columns_to_drop: Set[str],
                   nest: int = 2) -> Generator[str, None, None]:
     """
-    Return the list of intgroups
+    Return the list of groups of interest
     """
     cols = set(design.columns) - columns_to_drop
     return itertools.chain(
@@ -62,6 +87,24 @@ def get_intgroups(design: pandas.DataFrame,
         for i in range(1, nest + 1, 1)
         for j in itertools.permutations(cols, i)
     )
+
+
+def test_get_intgroups() -> None:
+    """
+    Test the above function get_intgroups
+    """
+    tested_data = pandas.DataFrame({
+        "my_factor": {"name1": "factor1", "name2": "factor1"},
+        "other_factor": "ok"}
+    )
+    tested = sorted(list(get_intgroups(tested_data, set("None"), 2)))
+    expected = sorted(['my_factor',
+        'other_factor',
+        'my_factor:other_factor',
+        'other_factor:my_factor'
+    ])
+    assert expected == tested
+
 
 
 def get_axes(max_axes: int = 10) -> Generator[List[int], None, None]:
@@ -73,3 +116,12 @@ def get_axes(max_axes: int = 10) -> Generator[List[int], None, None]:
         for a, b in itertools.combinations(range(1, max_axes + 1, 1), 2)
         if a + 1 == b
     ]
+
+
+def test_get_axes() -> None:
+    """
+    Test the function get_axes above
+    """
+    expected = [(1, 2), (2, 3), (3, 4)]
+    tested = get_axes(4)
+    assert expected == tested
