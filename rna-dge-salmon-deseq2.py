91b58a563802254494eb5602ae23a49f53031c04
tdayris-perso <tdayris@tutanota.de>
2020-12-07 13:03:06 +0100

    [dev] (wrapper): New launcher

---
+++ rna-dge-salmon-deseq2.py
@@ -0,0 +1,331 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+
+
+"""
+This is the CLI launcher of the rna-dge-salmon-deseq2 pipeline. This pipeline
+is powered by Snakemake and uses Salmon and FastQC on raw fastq files, then
+MultiQC on the results to aggregate quality reports.
+
+You will have quantified reads over both transcriptomic and genomic regions,
+alongside with complete quality reports.
+
+Please be aware that a Snakemake report is available for more details on
+both results and methods.
+
+If you have any question, please refer to the wiki at:
+https://github.com/tdayris-perso/rna-dge-salmon-deseq2/wiki
+
+Or open an issue at:
+https://github.com/tdayris-perso/rna-dge-salmon-deseq2/issues
+
+Citations:
+https://github.com/tdayris-perso/rna-dge-salmon-dese2/wiki/Pipeline_Content
+"""
+
+import argparse
+import os
+import logging
+import sys
+
+from pathlib import Path
+from snakemake.utils import makedirs
+from snakemake.shell import shell
+import snakemake
+from typing import Any
+
+try:
+    from scripts import prepare_config, prepare_design, common_script_rna_dge_salmon_deseq2
+except ImportError:
+    scripts_path = Path(os.path.realpath(__file__)).parent / "scripts"
+    sys.path.append(str(scripts_path))
+    import prepare_config, prepare_design, common_script_rna_dge_salmon_deseq2
+except ModuleNotFoundError:
+    scripts_path = Path(os.path.realpath(__file__)).parent / "scripts"
+    sys.path.append(str(scripts_path))
+    import prepare_config, prepare_design, common_script_rna_dge_salmon_deseq2
+
+
+
+def parser() -> argparse.ArgumentParser:
+    """
+    Build command line parser object
+    """
+    main_parser = argparse.ArgumentParser(
+        description=sys.modules[__name__].__doc__,
+    )
+
+    subparsers = main_parser.add_subparsers()
+    config = subparsers.add_parser(
+        "config",
+        parents=[prepare_config.parser()],
+        add_help=False
+    )
+    config.set_defaults(func=prepare_config.main)
+
+    design = subparsers.add_parser(
+        "design",
+        parents=[prepare_design.parser()],
+        add_help=False
+    )
+    design.set_defaults(func=prepare_design.main)
+
+    snake = subparsers.add_parser(
+        "snakemake",
+        add_help=True
+    )
+    snake.add_argument(
+        "--snakemake-args",
+        help="Snakemake arguments. If you use this wrapper instead "
+             "of real snakemake call, then please put all your"
+             " arguments in simple quotes. (default: %(default)s)",
+        type=str,
+        default=""
+    )
+    snake.add_argument(
+        "--no-profile",
+        help="Do not activate snakemake profile. This means you have to "
+             "define several environment variables. See documentation for "
+             "more information.",
+        action="store_true",
+        default=False
+    )
+    snake.add_argument(
+        "--no-cache",
+        help="Do not use the shared cache that allows user not to re-run "
+             "indexation steps. Not using cache will slow down your analysis "
+             "and use disk space for duplicated data.",
+        action="store_true",
+        default=False
+    )
+    snake.set_defaults(func=snakemake_run)
+
+    report_parser = subparsers.add_parser(
+        "report",
+        add_help=True
+    )
+    report_parser.add_argument(
+        "--snakemake-args",
+        help="Snakemake arguments. If you use this wrapper instead "
+             "of real snakemake call, then please put all your"
+             " arguments in simple quotes. (default: %(default)s)",
+        type=str,
+        default=""
+    )
+    report_parser.add_argument(
+        "--no-profile",
+        help="Do not activate snakemake profile. This means you have to "
+             "define several environment variables. See documentation for "
+             "more information.",
+        action="store_true",
+        default=False
+    )
+    report_parser.add_argument(
+        "--no-cache",
+        help="Do not use the shared cache that allows user not to re-run "
+             "indexation steps. Not using cache will slow down your analysis "
+             "and use disk space for duplicated data.",
+        action="store_true",
+        default=False
+    )
+
+    igr = subparsers.add_parser(
+        "flamingo",
+        add_help=True
+    )
+    igr.add_argument(
+        "--experimental-design",
+        help="Path to experimental design file (default: %(default)s)",
+        type=str,
+        default="exp-design.tsv"
+    )
+    igr.add_argument(
+        "--salmon-dir",
+        help="Path to samples quantified with Salmon (default: %(default)s)",
+        default=os.getcwd(),
+        type=str
+    )
+    igr.add_argument(
+        "--models-to-analyse",
+        help="A list of models. Each model is composed of [1] a"
+             "factor (a column name in your design, or a composition"
+             "of several columns separated by dots. See DESeq2"
+             "vignette.), [2] a numerator (your tested condition),"
+             "[3] a denominator (your reference condition), and [4]"
+             "a statistical formula. These values are separated by"
+             "commas. To include multiple models, please separate"
+             "them by spaces. See details on rna-dge-salmon-deseq2"
+             "wiki. (default: ['Condition,B,A,~Condition'])",
+        default=["Condition,A,B,~Condition"],
+        nargs="+"
+    )
+    igr.set_defaults(func=igr_run)
+
+    report_parser.set_defaults(func=report)
+    return main_parser
+
+
+def parse_args(args: Any) -> argparse.ArgumentParser:
+    """
+    This function parses command line arguments
+
+    Parameters
+        args     Any             All command line arguments
+
+    Return
+                ArgumentParser   A object designed to parse the command line
+
+    Example:
+    >>> parse_args(shlex.split("/path/to/fasta --no-fastqc"))
+    Namespace(aggregate=False, cold_storage=[' '], debug=False,
+    design='design.tsv', fasta='/path/to/fasta', gtf=None, libType='A',
+    no_fastqc=False, no_multiqc=False, quiet=False, salmon_index_extra='
+    --keepDuplicates --gencode --perfectHash', salmon_quant_extra='
+    --numBootstraps 100 --validateMappings --gcBias --seqBias',
+    singularity='docker://continuumio/miniconda3:4.4.10',
+    threads=1, workdir='.')
+    """
+    return parser().parse_args(args)
+
+
+
+def snakemake_command(opt: str = "",
+                      use_profile: bool = True,
+                      make_report: bool = False,
+                      use_cache: bool = True) -> str:
+    """
+    Build snakemake command line
+    """
+    return [
+        "snakemake",
+        f"-s {os.getenv('SNAKEFILE')}",
+        f"--profile {os.getenv('PROFILE')}" if use_profile is True else "",
+        "--report Quantification_Report.html" if make_report is True else "",
+        "--cache tr2gene" if use_cache is True else "",
+        opt
+    ]
+
+
+def run_cmd(*cmd_line) -> None:
+    """
+    Run a provided command line
+    """
+    cmd_line = " ".join(cmd_line)
+    print(cmd_line)
+    shell(cmd_line)
+
+def snakemake_run(cmd_line_args) -> None:
+    """
+    Call snakemake itself
+    """
+    command = snakemake_command(
+        opt=cmd_line_args.snakemake_args,
+        use_profile=not cmd_line_args.no_profile,
+        make_report=False,
+        use_cache=not cmd_line_args.no_cache
+    )
+
+    run_cmd(*command)
+
+
+def report(cmd_line_args) -> None:
+    """
+    Call snakemake itself
+    """
+    command = snakemake_command(
+        opt=cmd_line_args.snakemake_args,
+        use_profile=not cmd_line_args.no_profile,
+        make_report=True,
+        use_cache=not cmd_line_args.no_cache
+    )
+
+    run_cmd(*command)
+
+
+def igr_run(cmd_line_args) -> None:
+    """
+    Call this pipeline whole pipeline with default arguments
+    """
+    config_cmd = [
+        "python3",
+        os.getenv('DGE_LAUNCHER'),
+        "config",
+        os.getenv('GTF'),
+        "--models",
+        *cmd_line_args.models_to_analyse,
+        "--threads 20",
+        "--debug",
+        "--cold-storage /mnt/isilon /mnt/archivage",
+    ]
+    run_cmd(*config_cmd)
+
+    design_cmd = [
+        "python3",
+        os.getenv('DGE_LAUNCHER'),
+        "design",
+        cmd_line_args.salmon_dir,
+        "--import-design",
+        cmd_line_args.experimental_design,
+        "--debug"
+    ]
+    run_cmd(*design_cmd)
+
+    snakemake_cmd = [
+        "python3",
+        os.getenv("DGE_LAUNCHER"),
+        "snakemake"
+    ]
+    run_cmd(*snakemake_cmd)
+
+    report_cmd = [
+        "python3",
+        os.getenv("DGE_LAUNCHER"),
+        "report"
+    ]
+    run_cmd(*report_cmd)
+
+
+def run_cmd(*cmd_line) -> None:
+    """
+    Run a provided command line
+    """
+    cmd_line = " ".join(cmd_line)
+    if check_env():
+        print(cmd_line)
+        shell(cmd_line)
+    else:
+        print("Environment was not suitable for this pipeline to run.")
+
+
+def check_env() -> bool:
+    """
+    Verify environment variables required for this pipeline
+    """
+    expected = [
+        os.getenv('SNAKEFILE'),
+        os.getenv('PROFILE'),
+        os.getenv("DGE_LAUNCHER"),
+        os.getenv("GTF")
+    ]
+    print(expected)
+    test_if_none = all(var is not None for var in expected)
+    test_if_exists = all(os.path.exists(path) for path in expected)
+
+    return test_if_none and test_if_exists
+
+
+if __name__ == '__main__':
+    # Parsing command line arguments
+    args = parse_args(sys.argv[1:])
+    makedirs("logs/prepare")
+
+    # Build logging object and behaviour
+    logging.basicConfig(
+        filename="logs/prepare/design.log", filemode="w", level=logging.DEBUG
+    )
+
+    try:
+        args.func(args)
+    except Exception as e:
+        logging.exception("%s", e)
+        raise
---
+++ run.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -euic
+
+# This function only changes echo headers
+# for user's sake.
+function message() {
+  # Define local variables
+  local status=${1}         # Either INFO, CMD, ERROR or DOC
+  local message="${2:-1}"   # Your message
+
+  # Classic switch based on status
+  if [ ${status} = INFO ]; then
+    echo -e "\033[1;36m@INFO:\033[0m ${message}"
+  elif [ ${status} = ERROR ]; then
+    echo -e "\033[41m@ERROR:\033[0m ${message}"
+  elif [ ${status} = DOC ]; then
+    echo -e "\033[0;33m@DOC:\033[0m ${message}"
+  else
+    error_handling ${LINENO} 1 "Unknown message type"
+  fi
+}
+
+# This function will take error messages and exit the program
+function error_handling() {
+  # Geathering input parameter (message, third parameter is optionnal)
+  echo -ne "\n"
+  local parent_lineno="$1"
+  local code="$2"
+  local message="${3:-1}"
+
+  # Checking the presence or absence of message
+  if [[ -n "$message" ]] ; then
+    # Case message is present
+    message ERROR "Error on or near line ${parent_lineno}:\n ${message}"
+    message ERROR "Exiting with status ${code}"
+  else
+    # Case message is not present
+    message ERROR "Error on or near line ${parent_lineno}"
+    message ERROR "Exiting with status ${code}"
+  fi
+
+  # Exiting with given error code
+  exit "${code}"
+}
+
+function help_message() {
+  message DOC "Hi, thanks for using me as your script for running "
+  message DOC "rna-dge-salmon-deseq2 I'm very proud to be your script today,"
+  message DOC "and I hope you'll enjoy working with me."
+  echo ""
+  message DOC "Every time you'll see a line starting with '@', "
+  message DOC "it will be because I speak."
+  message DOC "In fact, I always start my speech with :"
+  message DOC "'\033[0;33m@DOC:\033[0m' when i't about my functions,"
+  message DOC "'\033[1;36m@INFO:\033[0m' when it's about my intentions, "
+  message DOC "'\033[41m@ERROR:\033[0m', I tell you when things go wrong."
+  echo ""
+  message DOC "I understand very fiew things, and here they are:"
+  message DOC "-h | --help        Print this help message, then exit."
+  message DOC "-i | --exp-design  Path to TSV-formatted experimental-design."
+  message DOC "                   default: exp-design.tsv"
+  message DOC "-m | --models      Four values, separated by commas:"
+  message DOC "                   (1) = the name of a column in the design."
+  message DOC "                   (2) = the tested confition."
+  message DOC "                   (3) = the reference condition."
+  message DOC "                   (4) = the R formula"
+  message DOC "                   Default: 'Condtion,B,A,~Condition'"
+  message DOC "-s | --salmon      Path to salmon quantification directory"
+  message DOC "                   default: ${PWD}"
+  message DOC "Otherwise, run me without any arguments and I'll do magic."
+  echo ""
+  message DOC "A typical command line would be:"
+  message DOC "bash /path/to/run.sh"
+  exit 0
+}
+
+[[ $# -gt 0 ]] && help_message
+
+# Loading conda
+message INFO "Sourcing conda for users who did not source it before."
+source "$(conda info --base)/etc/profile.d/conda.sh" && conda activate || exit error_handling "${LINENO}" 1 "Could not source conda environment."
+
+# Install conda environment if not installed before
+message INFO "Installing environment if and only if this action is needed."
+$(conda info --envs | grep "rna-dge-salmon-deseq2" > "/dev/null") && message INFO "Pipeline already installed! What a chance!" || conda env create --force -f "/mnt/beegfs/pipelines/rna-dge-salmon-deseq2/pipeline/rna-dge-salmon-deseq2/envs/workflow_flamingo.yaml"
+
+# Check on environment variables: if env are missing
+message INFO "Loading 'rna-dge-salmon-deseq2' environment"
+conda activate rna-dge-salmon-deseq2 || error_handling "${LINENO}" 2 "Could not activate the environment 'rna-dge-salmon-deseq2'."
+
+# then installation process did not work properly
+echo INFO "Running pipeline if and only if it is possible"
+$(export -p | grep "RNA_COUNT_LAUNCHER" --quiet) && python3 ${RNA_COUNT_LAUNCHER} flamingo || error_handling ${LINENO} 3 "Could not locate rna-dge-salmon-deseq2 launcher at: ${RNA_COUNT_LAUNCHER}"
